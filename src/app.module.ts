ad0db5ea564c9e43db7f2fc79864cc38a858e23a
msri23 <112919748+msri23@users.noreply.github.com>
2022-11-24 21:17:32 +0530

    user module and report module initial setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ReportsModule],
   controllers: [AppController],
   providers: [AppService],
 })
