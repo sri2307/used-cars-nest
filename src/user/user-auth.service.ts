f65808b825636b5638b0a7daf6fdd957130ffdda
msri23 <112919748+msri23@users.noreply.github.com>
2022-12-07 08:26:03 +0530

    sign up added

--- src/user/user-auth.service.ts
+++ src/user/user-auth.service.ts
@@ -3,10 +3,12 @@ import { badRequestException } from 'src/utils/exceptions';
 import { UserDto } from './dtos/user.dto';
 import { UserService } from './user.service';
 import { EXCEPTIONS } from 'src/utils/constants';
+import { promisify } from 'util';
+import { randomBytes, scrypt as _scrypt } from 'crypto';
 
 
 const {badException}=EXCEPTIONS.user
-
+const scrypt=promisify(_scrypt)
 @Injectable()
 export class UserAuthService {
     constructor(private userService:UserService){}
@@ -15,7 +17,11 @@ export class UserAuthService {
         const {email,password}=payload;
         const user=await this.userService.find(email)
         badRequestException(user,badException)
-        return user
+        const salt=randomBytes(8).toString('hex')
+        const hash=(await scrypt(salt,password,32)) as Buffer
+        const result=`${salt}.${hash.toString('hex')}`
+        const newUser=await this.userService.create({email,password:result})
+        return newUser
     }
 
 }
